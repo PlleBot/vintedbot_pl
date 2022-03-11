f42860043c93129fdd36700e05f7ba1036483a24
PlleBot <100094258+PlleBot@users.noreply.github.com>
2022-03-11 16:43:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,7 @@ import dataset
 
 db = dataset.connect('sqlite:///data/data.db')
 table = db['subscriptions']
-table.insert({
-        'url': 'rr',
-        'channel_id': 'rr',
-        'synced': False,
-        'last_sync': 'tes'
-    })
+
 
 for i in table:
     print(i)
